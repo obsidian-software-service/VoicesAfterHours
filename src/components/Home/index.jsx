6abc8089be6b5f7886f931c280014beded58db06
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-12 16:38:19 -0300

    spotify components

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, CardMedia } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import Iframe from '../Iframe'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     color: theme.palette.primary.contrastText,
+  },
+  cardMediaT: {
+    width: 300,
+    height: 380,
+  },
+  cardMediaB: {
+    width: 300,
+    height: 56,
+    border: 'none',
+    overflow: 'hidden',
   },
 }))
 
@@ -15,6 +26,31 @@ const Home = () => {
       <Typography variant="h2" className={classes.root}>
         Home
       </Typography>
+
+      <Iframe
+        src="https://open.spotify.com/embed/playlist/2geuBVoTTYRNKl407ZXBbQ"
+        className={classes.cardMediaT}
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      />
+
+      <Iframe
+        src="https://open.spotify.com/follow/1/?uri=spotify:user:11173597240?si=3b9fa8a7bd714564&size=detail&theme=dark"
+        className={classes.cardMediaB}
+        scrolling="no"
+        frameborder="0"
+        allowtransparency="true"
+      />
+
+      <Iframe
+        src="https://open.spotify.com/embed/track/0Z2OhR9OoUdZ9rhEy9ECkq"
+        width="300"
+        height="80"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      />
     </Grid>
   )
 }
---
+++ src/components/Iframe.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Iframe = ({...props}) => {
+  return (
+    <iframe {...props}>
+    </iframe>
+  )
+}
+
+export default Iframe

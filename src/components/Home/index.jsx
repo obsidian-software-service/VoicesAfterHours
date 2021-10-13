ae3de7799980938566c3f6fc3477811fa9a9da21
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-13 15:50:01 -0300

    solved request

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Typography, CardMedia } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import Iframe from '../Iframe'
 

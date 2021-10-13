3f59c3259257fdc0d434a234b26a7259aeaf3e0e
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-13 15:40:37 -0300

    upgrate spotify component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,50 +7,36 @@ const useStyles = makeStyles((theme) => ({
   root: {
     color: theme.palette.primary.contrastText,
   },
-  cardMediaT: {
-    width: 300,
-    height: 380,
-  },
-  cardMediaB: {
-    width: 300,
-    height: 56,
-    border: 'none',
-    overflow: 'hidden',
-  },
 }))
 
-const Home = () => {
+const Home = ({ spotifyUrl }) => {
   const classes = useStyles()
+
   return (
     <Grid container justify="center">
       <Typography variant="h2" className={classes.root}>
         Home
       </Typography>
 
-      <Iframe
-        src="https://open.spotify.com/embed/playlist/2geuBVoTTYRNKl407ZXBbQ"
-        className={classes.cardMediaT}
-        frameborder="0"
-        allowtransparency="true"
-        allow="encrypted-media"
-      />
+      <Grid container justify="center">
+        <Iframe
+          spotifyLink={
+            'https://open.spotify.com/playlist/2geuBVoTTYRNKl407ZXBbQ?si=8e35e9de1f6a4a01'
+          }
+          scrolling="no"
+          frameborder="0"
+          allowtransparency="true"
+        />
 
-      <Iframe
-        src="https://open.spotify.com/follow/1/?uri=spotify:user:11173597240?si=3b9fa8a7bd714564&size=detail&theme=dark"
-        className={classes.cardMediaB}
-        scrolling="no"
-        frameborder="0"
-        allowtransparency="true"
-      />
-
-      <Iframe
-        src="https://open.spotify.com/embed/track/0Z2OhR9OoUdZ9rhEy9ECkq"
-        width="300"
-        height="80"
-        frameborder="0"
-        allowtransparency="true"
-        allow="encrypted-media"
-      />
+        <Iframe
+          spotifyLink={
+            'https://open.spotify.com/track/614NVFo5csQqRnryl4jXs2?si=ccdc69b1ee224ad7'
+          }
+          scrolling="no"
+          frameborder="0"
+          allowtransparency="true"
+        />
+      </Grid>
     </Grid>
   )
 }
